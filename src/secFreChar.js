b3940b69647b788c88df9f47362d0d734639398a
shanmugapriya <shanmugapriya.k@mitrahsoft.com>
2025-05-07 13:43:34 +0530

    Updated the js concepts

---
+++ src/secFreChar.js
@@ -0,0 +1,23 @@
+
+//qn: second frequent char
+
+const a = "jjjaaaccr";
+let first=0
+let sec=0
+let ans=''
+for (let i = 0; i < a.length; i++) {
+  let count = 0;
+  for (let j = 0; j < a.length; j++) {
+    if (a[i] === a[j]) {
+      count = count + 1;
+    }
+}
+if(count>first){
+    sec=first
+    first=count
+}else if(count>sec && count !== first){
+    sec=count
+  ans=a[i]
+}
+}
+console.log(ans)
